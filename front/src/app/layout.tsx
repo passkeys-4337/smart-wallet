d194d6a3f52239777ed658804551a9577a680a1d
Benjamin A <97291322+Baoufa@users.noreply.github.com>
2023-10-26 15:22:35 +0200

    feat: add Radix, emotion.js and basic css styling (#11)
    * feat: add radix, theme and emotion
    
    * feat: add packages

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,20 +1,28 @@
-import '@rainbow-me/rainbowkit/styles.css'
-import { Providers } from './providers'
+import "@rainbow-me/rainbowkit/styles.css";
+import "@radix-ui/themes/styles.css";
+import { Providers } from "./providers";
+import { Theme, ThemePanel } from "@radix-ui/themes";
+import ThemeProvider from "@/components/ThemeProvider.tsx";
 
 export const metadata = {
-  title: 'wagmi',
-}
+  title: "wagmi",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <Providers>{children}</Providers>
+        <ThemeProvider attribute="class">
+          <Theme>
+            <Providers>{children}</Providers>
+            <ThemePanel />
+          </Theme>
+        </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
