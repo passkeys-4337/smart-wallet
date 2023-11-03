37dc847ecf856dbce1b72b9b97a9625e785148b2
Benjamin A <97291322+Baoufa@users.noreply.github.com>
2023-11-03 15:36:25 +0100

    feat: modal provider

---
+++ front/src/components/Modal2/index.tsx
@@ -0,0 +1,3 @@
+export default function Modal2() {
+  return <div>Modal2</div>;
+}
--- front/src/components/WCInput/index.tsx
+++ front/src/components/WCInput/index.tsx
@@ -4,15 +4,16 @@ import { useState } from "react";
 import { Flex, TextField, Button } from "@radix-ui/themes";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { useWalletConnect } from "@/libs/wallet-connect";
+import { useModal } from "@/providers/ModalProvider";
+import Modal1 from "../Modal1";
 
 export default function WCInput() {
   const [input, setInput] = useState<string>("");
 
   const { pairSession, pairingState } = useWalletConnect();
+  const { open } = useModal();
 
-  const isLoading = Object.values(pairingState).some(
-    (element) => element.isLoading
-  );
+  const isLoading = Object.values(pairingState).some((element) => element.isLoading);
 
   return (
     <Flex gap="2">
@@ -26,9 +27,8 @@ export default function WCInput() {
           onChange={(e) => setInput(e.target.value)}
         />
       </TextField.Root>
-      <Button onClick={() => pairSession(input)}>
-        {isLoading ? "is connecting" : "Connect"}
-      </Button>
+      <Button onClick={() => pairSession(input)}>{isLoading ? "is connecting" : "Connect"}</Button>
+      <Button onClick={() => open(<Modal1 />)}>SHOW MODAL</Button>
     </Flex>
   );
 }
