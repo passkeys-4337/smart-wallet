dda68469791427dd5765d411694366da0ac8375a
Benjamin A <benjamin.anoufa@gmail.com>
2023-11-15 19:05:29 +0100

    feat: allow copying address (#38)
    Co-authored-by: bigq <yum0e@protonmail.com>

---
+++ front/src/components/Address/index.tsx
@@ -0,0 +1,34 @@
+import { useMe } from "@/providers/MeProvider";
+import { CopyIcon } from "@radix-ui/react-icons";
+import { Button, Tooltip } from "@radix-ui/themes";
+import { CSSProperties, useState } from "react";
+
+type Props = {
+  style?: CSSProperties;
+};
+
+export default function Address(props: Props) {
+  const [isCopied, setIsCopied] = useState(false);
+  const { me } = useMe();
+
+  if (!me) {
+    return null;
+  }
+
+  return (
+    <Tooltip content="Copied!" open={isCopied}>
+      <Button
+        variant="soft"
+        onClick={() => {
+          navigator.clipboard.writeText(me?.account || "");
+          setIsCopied(true);
+          setTimeout(() => setIsCopied(false), 1000);
+        }}
+        style={{ ...props.style, display: "flex", alignItems: "center", gap: "6px" }}
+      >
+        <CopyIcon />
+        {me?.account.slice(0, 6)}...{me?.account.slice(-4)}
+      </Button>
+    </Tooltip>
+  );
+}
